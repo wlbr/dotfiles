13e6f6d9b62bd3b26bd69cab8eed0cabd107b757
Michael Wolber <mwolber@gmx.de>
2018-09-28 13:50:49 +0200

    Removed prettier extension.

--- vscode/extensions/fabiospampinato.vscode-todo-plus-4.4.4/out/src/views/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/* IMPORT */
-Object.defineProperty(exports, "__esModule", { value: true });
-const embedded_1 = require("./embedded");
-const files_1 = require("./files");
-/* EXPORT */
-exports.default = [files_1.default, embedded_1.default];
-//# sourceMappingURL=index.js.map
