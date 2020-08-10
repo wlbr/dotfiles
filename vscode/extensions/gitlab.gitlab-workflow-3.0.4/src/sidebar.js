9dc1be9a36e642d93517669bcd80114c886c5547
Michael Wolber <mwolber@gmx.de>
2020-08-10 10:15:00 +0200

    Removed vscode config.

--- vscode/extensions/gitlab.gitlab-workflow-3.0.4/src/sidebar.js
+++
@@ -1,9 +0,0 @@
-const vscode = require('vscode');
-
-const refresh = () => {
-  vscode.gitLabWorkflow.sidebarDataProviders.forEach(provider => {
-    provider.refresh();
-  });
-};
-
-exports.refresh = refresh;
--- vscode/extensions/znck.grammarly-0.12.5/fixtures/readme.md
+++
@@ -1,5 +0,0 @@
-# Example Document
-
-This document is an example document for testing Grammarly extension.
-
-An unknown word like inversifyjs can be added to the dictionary.
